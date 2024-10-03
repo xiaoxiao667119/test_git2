2b008b5887cfb92f194c479094bbfd9627423b67
xiaoxiao667119 <xiaoxiao667119@163.com>
2024-10-03 08:04:20 +0800

    Initial Commit

---
+++ test_git2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_git2
+//
+//  Created by JZ P on 2024/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_git2/test_git2App.swift
@@ -0,0 +1,17 @@
+//
+//  test_git2App.swift
+//  test_git2
+//
+//  Created by JZ P on 2024/10/3.
+//
+
+import SwiftUI
+
+@main
+struct test_git2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
